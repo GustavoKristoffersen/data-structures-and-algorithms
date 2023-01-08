5817a3905d251f0c08862966e6e3af5e351b71fe
Gustavo William <gustavowilliam0980@gmail.com>
2023-01-08 01:44:32 -0300

    feat: static array implementation in python

---
+++ data-structures/arrays/static-array/array.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class Array:
+  def __init__(self, capacity):
+    self.capacity = capacity
+    self.last_position = -1
+    self.values = np.empty(self.capacity, dtype=int)
+
+  # O(n)
+  def print(self):
+    if self.last_position == -1:
+      print('empty...')
+    else:
+      for i in range(self.last_position + 1):
+        print(f'{i} - {self.values[i]}')
+
+  # O(1)
+  def add(self, value):
+    # no space left
+    if self.last_position == self.capacity - 1:
+        return
+    self.last_position += 1
+    self.values[self.last_position] = value
+
+  # O(n)
+  def linear_search(self, value):
+      for i in range(self.last_position + 1):
+        if self.values[i] == value:
+          return i
+      return -1
+
+  # O(n)
+  def delete(self, value):
+    p = self.linear_search(value)
+    if p == -1:
+      return -1
+    for i in range(p, self.last_position):
+        self.values[i] = self.values[i + 1]
+    self.last_position -= 1
