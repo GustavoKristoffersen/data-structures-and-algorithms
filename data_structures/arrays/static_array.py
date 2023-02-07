f857cdcf2bd502af6bf8d4127d378a118a623cdf
Gustavo William <gustavowilliam0980@gmail.com>
2023-02-07 00:51:04 -0300

    refactor static array implementation

--- data_structures/arrays/static_array.py
+++ data_structures/arrays/static_array.py
@@ -1,16 +1,13 @@
-class Array:
+from algorithms.searching.linear_search.linear_search import linear_search
+
+
+class StaticArray:
+  # On static arrays, the size of it is declared at the time
+  # the arrays is created and can not be changed afterwards
   def __init__(self, capacity):
     self.capacity = capacity
     self.last_position = -1
     self.values = [None] * capacity
-
-  # O(n)
-  def print(self):
-    if self.last_position == -1:
-      print('empty...')
-    else:
-      for i in range(self.last_position + 1):
-        print(f'{i} - {self.values[i]}')
 
   # O(1)
   def add(self, value):
@@ -21,13 +18,6 @@ class Array:
     self.values[self.last_position] = value
 
   # O(n)
-  def linear_search(self, value):
-      for i in range(self.last_position + 1):
-        if self.values[i] == value:
-          return i
-      return -1
-
-  # O(n)
   def delete(self, value):
     p = self.linear_search(value)
     if p == -1:
@@ -35,3 +25,7 @@ class Array:
     for i in range(p, self.last_position):
         self.values[i] = self.values[i + 1]
     self.last_position -= 1
+
+  # O(n)
+  def linear_search(self, value):
+    return linear_search(self.values, value)
